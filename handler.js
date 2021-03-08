f0a8498926705924cc193da4833bf515c80e36fb
Danilo Spinelli <gunzip@users.noreply.github.com>
2021-03-08 14:26:23 +0000

    daje

--- handler.js
+++ handler.js
@@ -2,6 +2,6 @@ const fetch = require("node-fetch");
 
 module.exports = {
     main: function (event, context) {
-        return fetch("https://swapi.dev/api/people/1").then(res => res.json())
+        return "hello world!"
     }
 }
