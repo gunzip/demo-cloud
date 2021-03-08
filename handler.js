35fd7fb86b99e6cbb8a97e8fbe673532221cfb63
Danilo Spinelli <gunzip@users.noreply.github.com>
2021-03-08 14:40:52 +0000

    daje

--- handler.js
+++ handler.js
@@ -2,6 +2,6 @@ const fetch = require("node-fetch");
 
 module.exports = {
     main: function (event, context) {
-        return "hello world again!"
+        return "hello world again and again!"
     }
 }
